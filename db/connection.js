046b92fcdf9ad81970168ccc3065c2bdf8bff36a
Alex <alex_mccarrick@hotmail.com>
2021-01-15 15:54:21 +0000

    ready to deploy to heroku

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,18 @@
+const ENV = process.env.NODE_ENV || 'development';
 const knex = require('knex');
-const config = require('../knexfile');
+
+const config = 
+    ENV === 'production'
+    ? {
+        client: 'pg',
+        connection: {
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        }
+    }
+    : require('../knexfile');
 
 const connection = knex(config);
 
